e075e58e817aa14fb1222d76e1e72c2aacd0c000
sheng <Shengkun Cui>
2020-10-25 14:09:30 -0500

    first commit

---
+++ 2020_CARLA_challenge/agent_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /auto/bin/docker_env.sh
+python3 -m pip install ephem
+python3 -m pip install tabulate 
+python3 -m pip install -r  ./carla_project/requirements.txt
+export PORT=2000                                                    # change to port that CARLA is running on
+export ROUTES=leaderboard/data/routes_testing/route_00.xml  # change to desired route
+export TEAM_AGENT=image_agent.py                                    # no need to change
+# export TEAM_AGENT=auto_pilot.py
+export TEAM_CONFIG=epoch24.ckpt                                      # change path to checkpoint
+# export TEAM_CONFIG=sample_date
+export HAS_DISPLAY=1
+
